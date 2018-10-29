4a4a9747ffb3a90cc43ef44707398c294454e883
Naoki Sega <499108+nsega@users.noreply.github.com>
2018-10-29 17:09:55 +0900

    add the exercise's answers on a tour of go.

--- exercise-stringers.go
+++ exercise-stringers.go
@@ -9,14 +9,14 @@ func (ipAddr IPAddr) String() string {
 	return fmt.Sprintf("%d.%d.%d.%d", ipAddr[0], ipAddr[1], ipAddr[2], ipAddr[3])
 }
 
-func main(){
-	addrs:= map[string]IPAddr{
-		"loopback" : {127,0,0,1},
-		"googleDNS" : {8,8,8,8},
+func main() {
+	addrs := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
 	}
-	for n, a := range addrs {
-		fmt.Printf("%v: %v\n", n, a ) 
-	}
-	
-}
 
+	for n, a := range addrs {
+		fmt.Printf("%v: %v\n", n, a)
+	}
+
+}
